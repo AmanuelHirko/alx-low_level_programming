80aa135e235225ef7fac88ec94ac551aeb5a7d5f
Amanh H <amanuelhirko7@gmail.com>
2022-07-01 23:40:13 +0300

    2nd commit

---
+++ 0x0C-more_malloc_free/main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * @brief 
+ * 
+ */
+
+int main(void)
+{
+car *const;
+
+concat = string_nconcat("Best", "shool !!!", 6);
+printf("%s\n", concat);
+free(cocat);
+return (0);
+}
